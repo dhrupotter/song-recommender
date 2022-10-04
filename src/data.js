5383c4cfad58342d41204d911f8a117a6484c984
dhrupotter <112274439+dhrupotter@users.noreply.github.com>
2022-10-04 21:12:21 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,111 +1,108 @@
 export const data = [
   {
-    title: "Song 1",
+    title: "Aankhon Se Batana",
+    movie: "Aankhon Se Batana",
+    imgSrc:
+      "https://a10.gaanacdn.com/gn_img/albums/9MAWe7KyJe/AWe9Jw5wWy/size_l.jpg",
+    singer: "Dikshant",
+    genre: "Romance",
+    link: "https://www.youtube.com/watch?v=ZJ34geQLtUE"
+  },
+  {
+    title: "Rock On!!",
+    movie: "Rock On",
+    imgSrc:
+      "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/8903243255737/1648758352/srch_hungama_1466952.jpg",
+    singer: "Farhan Akhtar",
+    genre: "Rock",
+    link: "https://www.youtube.com/watch?v=EOyyVT31cCM"
+  },
+  {
+    title: "Naina",
+    movie: "Khoobsurat",
+    imgSrc: "https://i.scdn.co/image/ab67616d0000b2730fa193d01a315e3259d197cd",
+    singer: "Armaan Malik, Sona Mohapatra",
+    genre: "Sad",
+    link: "https://www.youtube.com/watch?v=kftFCL2bBbU"
+  },
+  {
+    title: "Moh Moh Ke Dhaage (Male)",
+    movie: "Dum Laga Ke Haisha",
+    imgSrc:
+      "https://a10.gaanacdn.com/gn_img/albums/BZgWoOW2d9/ZgWo9V4aK2/size_l.webp",
+    singer: "Papon",
+    genre: "Romance",
+    link: "https://www.youtube.com/watch?v=X4rEsj4z7MY"
+  },
+  {
+    title: "Saadda Haq",
+    movie: "Rockstar",
+    imgSrc:
+      "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/8903242676182/1615237527/srch_hungama_396882.jpg",
+    singer: "Mohit Chauhan",
+    genre: "Rock",
+    link: "https://www.youtube.com/watch?v=p9DQINKZxWE"
+  },
+  {
+    title: "Kesariya",
     imgSrc:
       "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
+    singer: "Arijit Singh, Pritam, Amitabh  Bhattacharya",
     movie: "Brahmastra",
     genre: "Romance",
-    link: "google.com"
+    link: "https://www.youtube.com/watch?v=BddP6PYo2gs"
   },
   {
-    title: "Song 2",
-    movie: "Brahmastra",
+    title: "Dil Dhadakne Do",
+    movie: "Zindagi Na Milegi Dobara",
     imgSrc:
-      "https://a10.gaanacdn.com/gn_img/albums/koMWQ4B3qL/oMWQO5LXKq/size_l.webp",
-    singer: "Dhruvi Gandhi",
+      "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/8903242664776/1615247430/srch_hungama_390990.jpg",
+    singer: "Joi Barua",
+    genre: "Rock",
+    link: "https://www.youtube.com/watch?v=WuMWwPHTSoY"
+  },
+  {
+    title: "Sunn Raha Hai",
+    movie: "Aashiqui 2",
+    imgSrc:
+      "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/8903236883039/1615212318/srch_hungama_1776391.jpg",
+    singer: "Ankit Tiwari",
+    genre: "Rock",
+    link: "https://www.youtube.com/watch?v=eHRrZ5DQCV4"
+  },
+  {
+    title: "Agar Tum Saath Ho",
+    movie: "Tamasha",
+    imgSrc:
+      "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/1444970739/16537841.jpg",
+    singer: "Alka Yagnik",
+    genre: "Sad",
+    link: "https://www.youtube.com/watch?v=xRb8hxwN5zc"
+  },
+  {
+    title: "Raataan Lambiyan",
+    movie: "Shershaah",
+    imgSrc:
+      "https://a10.gaanacdn.com/gn_img/song/lJvKa56KDV/vKa91Z1RKD/size_l_1628085090.webp",
+    singer: "Asees Kaur, Jubin Nautiyal, Tanishk Bagchi",
     genre: "Romance",
-    link: "google.com"
+    link: "https://www.youtube.com/watch?v=gvyUuxdRdR4"
   },
   {
-    title: "Song 3",
-    movie: "Brahmastra",
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
-    genre: "Romance",
-    link: "google.com"
-  },
-  {
-    title: "Song 4",
-    movie: "Brahmastra",
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
-    genre: "Rock",
-    link: "google.com"
-  },
-  {
-    title: "Song 5",
-    movie: "Brahmastra",
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
-    genre: "Rock",
-    link: "google.com"
-  },
-  {
-    title: "Song 6",
-
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    movie: "Brahmastra",
-    singer: "Dhruvi Gandhi",
-    genre: "Rock",
-    link: "google.com"
-  },
-  {
-    title: "Song 7",
-    movie: "Brahmastra",
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
-    genre: "Rock",
-    link: "google.com"
-  },
-  {
-    title: "Song 8",
-    movie: "Brahmastra",
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
-    genre: "Rock",
-    link: "google.com"
-  },
-  {
-    title: "Song 9",
-
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
+    title: "Tu Jaane Na",
+    movie: "Ajab Prem Ki Ghazab Kahani",
+    imgSrc: "https://i.scdn.co/image/ab67616d0000b2733fc744278f09205afd1e23c4",
     singer: "Dhruvi Gandhi",
     genre: "Sad",
-    link: "google.com"
+    link: "https://www.youtube.com/watch?v=53l2DkivxJs"
   },
   {
-    title: "Song 10",
-    movie: "Brahmastra",
+    title: "Main Rahoon Ya Na Rahoon",
+    movie: "Main Rahoon Ya Na Rahoon",
     imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
+      "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/8903431613820/1615228814/srch_hungama_16790305.jpg",
+    singer: "Armaan Malik",
     genre: "Sad",
-    link: "google.com"
-  },
-  {
-    title: "Song 11",
-    movie: "Brahmastra",
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Dhruvi Gandhi",
-    genre: "Sad",
-    link: "google.com"
-  },
-  {
-    title: "Song 12",
-    movie: "Brahmastra",
-    imgSrc:
-      "https://a10.gaanacdn.com/gn_img/song/z8k3yd1Krx/k3y4gl50Kr/size_l_1658037089.webp",
-    singer: "Soham Shah",
-    genre: "Sad",
-    link: "google.com"
+    link: "https://www.youtube.com/watch?v=Dp6lbdoprZ0"
   }
 ];
